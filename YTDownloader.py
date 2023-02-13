95d493a21e3de7caa4e67996c07cabc617e3f67f
Guillaume.I <101951542+Bolo101@users.noreply.github.com>
2023-02-13 15:22:17 +0000

    Update

--- YTDownloader.py
+++ YTDownloader.py
@@ -1,19 +1,23 @@
 from pytube import YouTube
 import os
 import sys
+import getpass
 state = True
+
+username = getpass.getuser()
+MusicPath = "/home/{}/Downloads/".format(username)
 
 while state:
     input1 = input("Entrer un lien youtube pour mp3 file: ")
     if input1 =="":
+        state = False
         sys.exit()
     else:
         vid = YouTube(input1)
         video = vid.streams.filter(only_audio=True).first()
-        destination = "PATH"
+        destination = MusicPath
         out_file = video.download(output_path=destination)
         base, ext = os.path.splitext(out_file)
         new_file = base + '.mp3'
         os.rename(out_file,new_file)
-        print(vid.title + " has been downladed")
-
+        print(vid.title + " has been downloaded")
