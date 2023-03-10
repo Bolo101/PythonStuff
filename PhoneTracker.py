91d3c834d87459f0d532bd0bfc8cef84a9c686c4
bololo7 <101951542+bololo7@users.noreply.github.com>
2023-03-10 21:58:41 +0100

    Create

--- PhoneTracker.py
+++ PhoneTracker.py
@@ -1,2 +1,17 @@
+#!/bin/python
 import phonenumbers
+from phonenumbers import carrier, geocoder, timezone
 
+mobileNumber = input("Entrer le numéro du mobile avec le code du pays: ")
+mobileNumber = phonenumbers.parse(mobileNumber)
+
+#timezone
+print(timezone.time_zones_for_number(mobileNumber))
+#carrier
+print(carrier.name_for_number(mobileNumber,"en"))
+#location
+print(geocoder.description_for_number(mobileNumber,"en"))
+#phone number validation
+print("Validating: ", phonenumbers.is_valid_number(mobileNumber))
+#Check possibility
+print("Checking possibilitity for",mobileNumber,": ",phonenumbers.is_possible_number)
