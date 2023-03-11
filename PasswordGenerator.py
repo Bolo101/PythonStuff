bc3d2291f10ef0654e32e2264be5bea7dad188ef
bololo7 <101951542+bololo7@users.noreply.github.com>
2023-03-11 22:07:12 +0100

    Updated

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -3,19 +3,20 @@
 import secrets
 import string
 
+#generate used characters
 letters = string.ascii_letters
 digits = string.digits
 specials = string.punctuation
 alphabet = letters + specials + digits
+#choose the lenght of the password
 pwd_lenght = int(input("Longueur du mot de passe: "))
-pwd = ''
-for i in range(pwd_lenght):
-    pwd +=''.join(secrets.choice(alphabet))
-
+#generate password until conditions are matched
 while True:
     pwd = ''
+    #generate password
     for i in range(pwd_lenght):
         pwd +=''.join(secrets.choice(alphabet))
+    #check if there are enought special caracters and digits
     if (sum(char in specials for char in pwd) >=3 and sum(char in digits for char in pwd) >=2):
         break
 
